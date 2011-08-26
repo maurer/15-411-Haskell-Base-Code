206bbae594671a80a8e183e789e6d227ba04eac1
Matthew Maurer <maurer@ugcs.caltech.edu>
2011-08-26 00:12:06 -0400

    Filled out the AST

--- src/Compile/Types/AST.hs
+++ src/Compile/Types/AST.hs
@@ -1,3 +1,14 @@
 module Compile.Types.AST where
 
-data AST = AST deriving Show
+import Text.ParserCombinators.Parsec.Pos (SourcePos)
+
+import Compile.Types.Ops
+
+data AST = Block [Decl] [Stmt] SourcePos deriving Show
+data Decl = Decl String SourcePos deriving Show
+data Stmt = Asgn String AsgnOp Expr SourcePos deriving Show
+data Expr = Const Int SourcePos
+          | Ident String SourcePos
+          | BinOp Op Expr Expr SourcePos
+          | UnOp Op Expr SourcePos deriving Show
+type AsgnOp = Maybe Op
